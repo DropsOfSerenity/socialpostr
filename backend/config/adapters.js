6f253dca6cfc968e5ab2f010cd70d8c8522c0962
Justin Schiff <AmbientRevolution@gmail.com>
2015-02-22 15:47:30 -0800

    used mongo, functionality is complete, baller

---
+++ backend/config/adapters.js
@@ -0,0 +1,10 @@
+module.exports.adapters = {
+  mongo: {
+    module: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    user: '',
+    password: '',
+    database: 'socialpostr'
+  }
+};
